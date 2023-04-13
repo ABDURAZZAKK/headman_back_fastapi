0653888c341351aaa07e33bf6eb7da29054534d7
abuad <abdurazzakk.m@gmail.com>
2023-04-14 00:02:31 +0300

    Изменил .env файл убрал лишнее

--- core/config.py
+++ core/config.py
@@ -4,16 +4,11 @@ from dotenv import load_dotenv, find_dotenv
 
 load_dotenv(find_dotenv())
 
-# POSTGRES
-PG_USER = os.getenv('POSTGRES_USER')
-PG_PASS = os.getenv('POSTGRES_PASSWORD')
-PG_HOST = os.getenv('POSTGRES_HOST')
-PG_PORT = os.getenv('POSTGRES_PORT')
-PG_DB = os.getenv('POSTGRES_DB')
 
 # URLs
 # "postgresql://root:root@localhost:5433/HEADMAN_DB"
-DATABASE_URL=f"postgresql://{PG_USER}:{PG_PASS}@{PG_HOST}:{PG_PORT}/{PG_DB}"
+DATABASE_URL=os.getenv('PG_DATABASE_URL')
+TEST_DATABASE_URL=os.getenv('TEST_PG_DATABASE_URL')
 
 # PATHs
 HW_ATTACHED_PATH='media\\hw_attached'
