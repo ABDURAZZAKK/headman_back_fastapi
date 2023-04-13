2c47dab76836b68a38d657aed4f60866d4bdbdc0
abuad <abdurazzakk.m@gmail.com>
2023-04-14 00:03:54 +0300

    Поменял тип поля status с str на HWStatus

--- models/homework.py
+++ models/homework.py
@@ -4,7 +4,7 @@ from enum import Enum
 
 
 
-class HWStatus(Enum):
+class HWStatus(str, Enum):
     OPEN = "открыто" 
     CLOSE = "закрыто"
 
@@ -13,42 +13,21 @@ class HWStatus(Enum):
 class Homework(BaseModel):
     id: int
     task: str
-    status: str
+    status: HWStatus
     path_to_files: str | None
     category_id : int
     creater: int
     created_at: datetime
     updated_at: datetime
 
-    @validator('status')
-    def status_v(cls, v: str) -> str:
-        if v.lower() in [i.value for i in HWStatus]:
-            return v.title()
-        raise ValueError('Invalid status. It should be "Закрыто" or "Открыто"')
-
-
 
 class HomeworkIn(BaseModel):
     task: str
-    status: str
+    status: HWStatus
     category_id:int
-    
-
-    @validator('status')
-    def status_v(cls, v: str) -> str:
-        if v.lower() in [i.value for i in HWStatus]:
-            return v.title()
-        raise ValueError('Invalid status. It should be "Закрыто" or "Открыто"')
 
 
 class HomeworkUpdate(BaseModel):
     task: str|None
-    status: str|None
+    status: HWStatus|None
     category_id:int|None
-    
-
-    @validator('status')
-    def status_v(cls, v: str, values) -> str:
-        if not 'status' is None and v.lower() in [i.value for i in HWStatus]:
-            return v.title()
-        raise ValueError('Invalid status. It should be "Закрыто" or "Открыто"')
