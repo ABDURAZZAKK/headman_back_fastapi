c27d4c6dbff15625589083aad3269d888adca54d
abuad <abdurazzakk.m@gmail.com>
2023-11-03 15:26:30 +0300

    добавил корсы подулючил энпоинты чата

--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
-from fastapi.responses import HTMLResponse
+from fastapi.responses import RedirectResponse
 
-from endpoints import users, auth, groups, categories, homeworks, points
+from endpoints import users, auth, groups, categories, homeworks, points, chat
+from fastapi.middleware.cors import CORSMiddleware
 
+
+origins = [
+    "http://localhost",
+]
 
 app = FastAPI(title="Headman FastAPI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 main_router = APIRouter()
 
 main_router.include_router(auth.router, prefix="/auth", tags=["auth"])
@@ -13,11 +28,12 @@ main_router.include_router(users.router, prefix="/users", tags=["users"])
 main_router.include_router(groups.router, prefix="/groups", tags=["groups"])
 main_router.include_router(categories.router, prefix="/categories", tags=["categories"])
 main_router.include_router(homeworks.router, prefix="/homeworks", tags=["homeworks"])
-main_router.include_router(points.router, prefix="/services", tags=["points"])
+main_router.include_router(points.router, prefix="/points", tags=["points"])
+main_router.include_router(chat.router, prefix="/chat", tags=["chat"])
 
 app.include_router(main_router, prefix="/api")
 
-
+app.include_router(chat.router, prefix="/chat")
 # @app.on_event("startup")
 # async def startup():
 #     await database.connect()
@@ -28,15 +44,9 @@ app.include_router(main_router, prefix="/api")
 #     await database.disconnect()
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=RedirectResponse)
 async def read_items():
-    return """
-    <html>
-        <body bgcolor="#1c1c1c">
-            <p color="white"><a href="http://127.0.0.1:8000/docs">FastAPI DOCS</a></p>
-        </body>
-    </html>
-    """
+    return RedirectResponse("/docs")
 
 
 if __name__ == "__main__":
